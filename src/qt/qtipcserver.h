0ed27a06e89de52f97f859ac5449ee6b40926916
blackmagiccoin <blackmagic@scryptmail.com>
2016-09-11 14:06:46 +0200

    git

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define BlackMagicCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BlackMagicCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
